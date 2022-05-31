67c0c89a1aa312c85af3c43236ba47feaf68f8fb
skyve2012 <hongyushen21@hotmail.com>
2022-05-31 11:57:46 +0800

    term swap version, just need to reduce term swap size

---
+++ contracts/interfaces/ITWAMMTermSwapOut.sol
@@ -0,0 +1,47 @@
+// // SPDX-License-Identifier: GPL-3.0-or-later
+
+// pragma solidity ^0.8.9;
+
+// interface ITWAMMTermSwapOut {
+    
+
+//     function cancelTermSwapTokenToToken(
+//         address token0,
+//         address token1,
+//         uint256 orderId,
+//         uint256 deadline
+//     ) external;
+
+//     function cancelTermSwapTokenToETH(
+//         address token,
+//         uint256 orderId,
+//         uint256 deadline
+//     ) external;
+
+//     function cancelTermSwapETHToToken(
+//         address token,
+//         uint256 orderId,
+//         uint256 deadline
+//     ) external;
+
+//     function withdrawProceedsFromTermSwapTokenToToken(
+//         address token0,
+//         address token1,
+//         uint256 orderId,
+//         uint256 deadline
+//     ) external;
+
+//     function withdrawProceedsFromTermSwapTokenToETH(
+//         address token,
+//         uint256 orderId,
+//         uint256 deadline
+//     ) external;
+
+//     function withdrawProceedsFromTermSwapETHToToken(
+//         address token,
+//         uint256 orderId,
+//         uint256 deadline
+//     ) external;
+
+   
+// }
