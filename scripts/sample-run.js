f5d1540824c9a78ede301ed6ff5bc96d136cf762
skyve2012 <hongyushen21@hotmail.com>
2022-07-31 21:20:46 +0800

    merge

--- scripts/sample-run.js
+++ scripts/sample-run.js
@@ -32,16 +32,16 @@ async function main() {
   const token1 = await ethers.getContractAt("ERC20Mock", token1Addr);
 
   // loading necessary contracts
-  const TWAMMAddr = "0xDb0F56C376fb178c1f1629374ADE3E5cECcF69D3";
+  const TWAMMAddr = "0x7c0f35D765C7201E95DD1DC9760DF16cb8EeE80f";
   const twamm = await ethers.getContractAt("TWAMM", TWAMMAddr);
 
-  const TWAMMLiquidityAddr = "0xE5bDf3dFFd442Bff6Aee60a79F0168bab54813c0";
+  const TWAMMLiquidityAddr = "0x0CcAcd0D37DA78525D8A3c9e424a0669c9F25098";
   const twammLiquidity = await ethers.getContractAt("TWAMMLiquidity", TWAMMLiquidityAddr);
 
-  const TWAMMInstantSwapAddr = "0xd6dDdD0542e6960f0C0d851333a5DD215F2CBdA8";
+  const TWAMMInstantSwapAddr = "0x1C1674a19C47085A30B6a7B5827d3b6d9659cd97";
   const twammInstantSwap = await ethers.getContractAt("TWAMMInstantSwap", TWAMMInstantSwapAddr);
 
-  const TWAMMTermSwapAddr = "0x7933583Fe13EAB71Db1C92cfc6C1F2596BDDCb3e";
+  const TWAMMTermSwapAddr = "0xC7CA07D4478Ff155b8979524BBF11AED0651324F";
   const twammTermSwap = await ethers.getContractAt("TWAMMTermSwap", TWAMMTermSwapAddr);
 
   const sleep = ms => new Promise(res => setTimeout(res, ms));
